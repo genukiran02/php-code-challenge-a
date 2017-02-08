5b256a36a1b783d1d1b70d263af6b0646ea41224
Hugo Vacher <korrigan33@gmail.com>
2017-02-08 16:47:49 -0500

    Added weather paths

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php
 
 require('vendor/autoload.php');
-
-$adapter = new \Ivory\HttpAdapter\Guzzle6HttpAdapter();
-$geocoder = new \Korri\Geocoder\Provider\IpApi($adapter);
-
-print_r($geocoder->geocode('66.130.182.186'));
