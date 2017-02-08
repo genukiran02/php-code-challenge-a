047d212d7212420fc082db69a38081823612dafd
Hugo Vacher <korrigan33@gmail.com>
2017-02-08 17:47:31 -0500

    Exit after transmiting HTML

--- index.php
+++ index.php
@@ -3,6 +3,18 @@
 require('vendor/autoload.php');
 
 //We will show the Swagger UI on / for now
-if(isset($_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_URI'] == '/') {
+if (isset($_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_URI'] == '/') {
     readfile(__DIR__ . '/static/swagger-ui/index.html');
+    exit;
 }
+
+
+//init controller
+$adapter = new \Ivory\HttpAdapter\Guzzle6HttpAdapter();
+$geolocation = new \Korri\Requester\Geolocation($adapter);
+
+$server = new Jacwright\RestServer\RestServer('debug');
+$server->refreshCache(); // uncomment momentarily to clear the cache if classes change in production mode
+
+$server->addClass(new Korri\Controller($geolocation));
+$server->handle();
