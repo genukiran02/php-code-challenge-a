9498ab070457b0c3ebaabde2e4640f2c58a67129
Hugo Vacher <korrigan33@gmail.com>
2017-02-08 16:43:46 -0500

    Added geolocation paths

--- index.php
+++ index.php
@@ -2,3 +2,7 @@
 
 require('vendor/autoload.php');
 
+$adapter = new \Ivory\HttpAdapter\Guzzle6HttpAdapter();
+$geocoder = new \Korri\Geocoder\Provider\IpApi($adapter);
+
+print_r($geocoder->geocode('66.130.182.186'));
