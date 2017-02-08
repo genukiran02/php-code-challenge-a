387046b3064018cbe5567e3ef5606177ed315d53
Hugo Vacher <korrigan33@gmail.com>
2017-02-08 17:03:35 -0500

    Server swagger UI and fix static files path

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
 
 require('vendor/autoload.php');
+
+//We will show the Swagger UI on / for now
+if(isset($_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_URI'] == '/') {
+    readfile(__DIR__ . '/static/swagger-ui/index.html');
+}
